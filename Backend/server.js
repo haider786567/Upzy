7416f0e21344720d83b16b5068911f8c6a8ce432
Haider <Haiderkg78@gmail.com>
2026-05-02 13:17:34 +0530

    socker io connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,19 @@
+import http from "http";
 import app from "./src/app.js";
 import config from "./src/config/config.js";
 import connectDB from "./src/config/db.js";
+import { initSocket } from "./src/socket/socket.js";
 
-// Connect to the database
+// Connect DB
 connectDB();
 
-// Start the server
+// 🔥 Create HTTP server
+const server = http.createServer(app);
 
+// 🔥 Attach socket
+initSocket(server);
 
-
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
+// Start server
+server.listen(config.PORT, () => {
+  console.log(`🚀 Server running on port ${config.PORT}`);
 });
