18c0f9b1fe5961641023ea37bb29c0b0db5cf273
Haider <Haiderkg78@gmail.com>
2026-05-02 23:39:48 +0530

    auth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
 
 export default app;
