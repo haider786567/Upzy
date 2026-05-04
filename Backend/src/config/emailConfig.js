b831343955fd3e8a80a839de627576a441d68740
Haider <Haiderkg78@gmail.com>
2026-05-04 09:20:45 +0530

    feat: update email configuration to use config module for credentials

--- Backend/src/config/emailConfig.js
+++ Backend/src/config/emailConfig.js
@@ -1,11 +1,12 @@
 import nodemailer from "nodemailer";
+import config from "./config.js";
 const transporter = nodemailer.createTransport({
   host: "142.250.183.108", // Gmail IPv4
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS
+    user: config.EMAIL,
+    pass: config.EMAIL_PASS
   },
   tls: {
     servername: "smtp.gmail.com"
